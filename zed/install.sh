c0c3a03191882098375f567b5fc86d2952fd123e
Hans Jakob Emmel <hans.emmel@gmail.com>
2024-07-11 23:34:40 +1000

    fix: tasks

--- zed/install.sh
+++ zed/install.sh
@@ -4,6 +4,6 @@ set -e
 
 mkdir -p ${HOME}/.config/zed/
 ln -sf "${PWD}/zed/settings.json" "${HOME}/.config/zed/settings.json"
-ln -sf "${PWD}/zed/keybindings.json" "${HOME}/.config/zed/keymap.json"
+ln -sf "${PWD}/zed/tasks.json" "${HOME}/.config/zed/tasks.json"
 mkdir -p ${HOME}/.config/zed/themes/
 ln -sf "${PWD}/zed/themes/dracula_pro.json" "${HOME}/.config/zed/themes/keymap.json"
